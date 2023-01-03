9086b40668ae29beaa5273ce66f7e501108af990
CodeWithKaushal023 <kpandey023@gmail.com>
2023-01-03 18:27:53 +0530

    add server update

--- index.js
+++ index.js
@@ -29,24 +29,24 @@ app.post('/studentsAdd', jsonParser, async (req, res) => {
 
 })
 
-app.get('/studentList',async (req,res)=>{
+app.get('/studentList', async (req, res) => {
     const studentList = await Students.find({});
     res.send(studentList)
 })
 
-app.delete('/delete-student/:_id',async (req,res)=>{
-    const studentDel = await Students.deleteOne({_id:req.params._id});
+app.delete('/delete-student/:_id', async (req, res) => {
+    const studentDel = await Students.deleteOne({ _id: req.params._id });
     res.send(studentDel)
 })
 
 
-app.get('/studentView/:_id',async (req,res)=>{
-    const studentView = await Students.findOne({_id:req.params._id});
+app.get('/studentView/:_id', async (req, res) => {
+    const studentView = await Students.findOne({ _id: req.params._id });
     res.send(studentView)
 })
 
 
-app.put('/studentEdit/:_id',jsonParser, async (req,res)=>{
+app.put('/studentEdit/:_id', jsonParser, async (req, res) => {
 
     let result = await Students.updateOne(
         { _id: req.params._id },
