c12f629a7d894c77ab194f157ace6c132566a199
CodeWithKaushal023 <kpandey023@gmail.com>
2023-01-04 12:36:32 +0530

    mern

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.connect('mongodb+srv://kkpweb023:KkpP831722@cluster0.a9anlpj.mongodb.net/Crud?retryWrites=true&w=majority',
+/*
+module.exports = mongoose.connect('mongodb+srv://kkpweb023:KkpP831722@cluster0.a9anlpj.mongodb.net/Crud?retryWrites=true&w=majority',*/
+
+
+module.exports = mongoose.connect('mongodb+srv://kkpweb023:KkpP831722@cluster0.a9anlpj.mongodb.net/MERN?retryWrites=true&w=majority',
 {
     useNewUrlParser:true,
     useUnifiedTopology:true
